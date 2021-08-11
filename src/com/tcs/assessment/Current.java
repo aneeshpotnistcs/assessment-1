8394a66d1883d42165e659c1329fcb44180b03b6
aneeshpotnistcs <aneeshpotnistcs@gmail.com>
2021-08-11 12:45:03 +0000

    updated files of savings and current

--- src/com/tcs/assessment/Current.java
+++ src/com/tcs/assessment/Current.java
@@ -6,14 +6,57 @@ public class Current extends Account {
 
 	private Date createDate;
 	private float balance = 500;
-	private String type;
+	private boolean type;
 	private String status;
 	private float overdraftBalance;
 
-	public static void main(String[] args) {
-		Current account = new Current();
-		System.out.println(account.ownnerName);
-		System.out.println(account.balance);
+	public Current(Date createDate, float balance, boolean type, String status, float overdraftBalance) {
+		super();
+		this.createDate = createDate;
+		this.balance = balance;
+		this.type = type;
+		this.status = status;
+		this.overdraftBalance = overdraftBalance;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+
+	public boolean isType() {
+		return type;
+	}
+
+	public void setType(boolean type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public float getOverdraftBalance() {
+		return overdraftBalance;
+	}
+
+	public void setOverdraftBalance(float overdraftBalance) {
+		this.overdraftBalance = overdraftBalance;
 	}
 
 }
--- src/com/tcs/assessment/Savings.java
+++ src/com/tcs/assessment/Savings.java
@@ -6,13 +6,47 @@ public class Savings extends Account {
 
 	private Date createDate;
 	private float balance = 1000;
-	private String type;
+	private boolean type;
 	private String status;
 
-	public static void main(String[] args) {
-		Savings account = new Savings();
-		System.out.println(account.ownnerName);
-		System.out.println(account.balance);
+	public Savings(Date createDate, float balance, boolean type, String status) {
+		super();
+		this.createDate = createDate;
+		this.balance = balance;
+		this.type = type;
+		this.status = status;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+
+	public boolean isType() {
+		return type;
+	}
+
+	public void setType(boolean type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 }
